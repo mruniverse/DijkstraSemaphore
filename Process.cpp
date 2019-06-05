bae52cb4654cca8374f4ef712b7f10fe7c6eeea8
MrUniverse <yurisannin@hotmail.com>
2019-06-05 00:37:41 -0300

    Version 1.1
    inclusion_routine and exclusion_routine are working with tests.

--- Process.cpp
+++ Process.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
 using namespace std;
 
 typedef struct Process{
@@ -7,3 +12,68 @@ typedef struct Process{
   int state; //0 for incomplete and 10 for complete
 
 }Process;
+
+vector<int> inclusion_routine(Process p, vector<int> vector){
+  int rand_value;
+  srand (time(NULL));
+
+  cout << "Região crítica acessada." << endl;
+  for (int i = 1; i <= 10; i++) {
+    rand_value = rand() % 1000+1;
+
+    // vector.push_back(rand_value);
+    int last = vector.size();
+    if(vector[last] == 0){
+      vector[last] = rand_value;
+    }
+    else{
+      while(vector[last] != 0){
+        // cout << "teste" << endl;
+        last--;
+      }
+      vector[last] = rand_value;
+    }
+
+    p.state = i; //For each cycle, state will change and be recorded.
+    sleep(1);
+    cout << "Processo " << p.name << ": "<< i << " inserção" << endl;
+    cout << "Valor: " << rand_value << endl;
+  }
+
+  return vector;
+}
+
+vector<int> exclusion_routine(Process p, vector<int> vector){
+  cout << "Região crítica acessada." << endl;
+
+  for (int i = 1; i <= 10; i++) {
+    int last = vector.size();
+    cout << "Processo " << p.name << ": "<< i << " remoção" << endl;
+
+    if(vector[last] == 0){
+      cout << "Vetor vazio" << endl;
+    }
+    else{
+      while(vector[last-1] != 0){
+        // cout << "teste" << endl;
+        last--;
+      }
+      cout << "Valor: " << vector[last] << endl;
+      vector[last] = 0;
+    }
+
+    p.state = i; //For each cycle, state will change and be recorded.
+    sleep(1);
+  }
+
+  return vector;
+}
+
+void print(vector <int> const &a) {
+   cout << "The vector elements are : ";
+
+   for(int i=0; i < a.size(); i++){
+     cout << a.at(i) << ' ';
+   }
+   cout << endl;
+}
--- Semaphore.cpp
+++ Semaphore.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <stdlib.h>
+#include <time.h>
 #include "Process.cpp"
 
 using namespace std;
 
 int main() {
-  queue<Process> p_row;
-  queue<Process> blocked_row;
-  int s;
   Process p1,p2,p3,p4,p5,p6,p7,p8,p9,p10;
+  queue<Process> blocked_row;
+  queue<Process> p_row;
+  vector<int> vector;
+  int s;
+
+  vector.assign(1000, 0); //1000 int with value of 0
 
   p1.name = "p1"; p1.pid = 1; p1.state = 0;
-  p2.name = "p2"; p1.pid = 2; p1.state = 0;
+  p2.name = "p2"; p2.pid = 2; p2.state = 0;
+  p3.name = "p3"; p3.pid = 3; p3.state = 0;
+  p4.name = "p4"; p4.pid = 4; p4.state = 0;
+  p5.name = "p5"; p5.pid = 5; p5.state = 0;
+  p6.name = "p6"; p6.pid = 6; p6.state = 0;
+  p7.name = "p7"; p7.pid = 7; p7.state = 0;
+  p8.name = "p8"; p8.pid = 8; p8.state = 0;
+  p9.name = "p9"; p9.pid = 9; p9.state = 0;
+  p10.name = "p10"; p10.pid = 10; p10.state = 0;
+
   p_row.push(p1);
   p_row.push(p2);
-  // p_row.pop();
+  // // p_row.pop(); //When "pop" is used, the front is removed
+  //
+  // cout << p_row.front().name << endl;
+  // cout << p_row.back().name << endl;
+  // cout << p_row.size() << endl;
 
-  cout << p_row.front().name << endl;
-  cout << p_row.back().name << endl;
-  cout << p_row.size() << endl;
-
-  
+  vector = inclusion_routine(p1, vector);
+  print(vector);
+  vector = exclusion_routine(p1, vector);
+  print(vector);
+  // cout << vector.size();
 
   return 0;
 }
